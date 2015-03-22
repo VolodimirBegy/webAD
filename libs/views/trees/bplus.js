656b0af73fceb55433e1ebb9415657f6793beb12
Begy Volodimir <begy.volodimir@gmail.com>
2015-03-22 16:29:32 +0100

    bplus view

--- libs/views/trees/bplus.js
+++ libs/views/trees/bplus.js
@@ -293,7 +293,7 @@ BPlusTreeView.prototype.draw=function(con){
 	var w=lastX+50*this.scale;
 	var h=lastY+50*this.scale
 	
-	if(h<700)h=500;
+	if(h<500)h=500;
 	if(w<1000)w=1000;
 	
 	var stage = new Kinetic.Stage({
