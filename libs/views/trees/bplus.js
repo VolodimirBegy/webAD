5b5d830df2557e7ed880c68b40a6c6b49b8c7309
bplusAK <34027891+bplusAK@users.noreply.github.com>
2018-01-30 19:58:34 +0100

    Add files via upload

--- libs/views/trees/bplus.js
+++ libs/views/trees/bplus.js
@@ -1,8 +1,11 @@
 
-function BPlusTreeView(mod, cont, contSec){
+function BPlusTreeView(mod, cont, prot){
 	this.scale=1;
 	this.model=mod;
-	this.elementDisplay = contSec;
+	this.protDisplay = prot;
+	this.acVal;
+	this.acOp;
+
 
 	this.stage = new Kinetic.Stage({
   		container: cont,
@@ -15,15 +18,24 @@ function BPlusTreeView(mod, cont, contSec){
 
 BPlusTreeView.prototype.zoomIn=function(){
 	if(this.scale<3)this.scale=this.scale+0.1;
-	if(!this.model.runningOp) this.draw();
+	if(!this.model.runningOp) this.draw(undefined, -1, -1);
+	else{
+		this.model.update();
+		this.draw(undefined, this.acVal, this.acOp);
+	}
+	
 }
 
 BPlusTreeView.prototype.zoomOut=function(){
   	if(this.scale>0.5)this.scale=this.scale-0.1;
-	if(!this.model.runningOp) this.draw();
+	if(!this.model.runningOp) this.draw(undefined, -1, -1);
+	else{
+		this.model.update();
+		this.draw(undefined, this.acVal, this.acOp);
+	}
 }
 
-BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
+BPlusTreeView.prototype.draw=function(actNode, actualValue, operation){
 	
 	var pointerSpace =10*this.scale;
 	var cellWidth= 50*this.scale;
@@ -36,6 +48,7 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 	var layer = new Kinetic.Layer();
 	var clr = "black";
 	var moveSpeed=this.model.speed;
+	var pauseTime=1000;
 	var actVal=parseInt(actualValue);
 	var op=parseInt(operation); // 1 = add, 2 = delete, 3 = search, 4=double value
 	
@@ -43,13 +56,18 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 	var helpNode=undefined;
 	var i=0, j=0;
 
+	this.acVal=actVal;
+	this.acOp=op;
+
 	
 	if(this.model.root!=undefined) tmpNodes.push(this.model.root); // Wurzel hinzufuegen
+	if(this.model.root.rightPointer!=undefined) tmpNodes.push(this.model.root.rightPointer);
 	while(tmpNodes[i]!=undefined){ // uebrige Knoten hinzu
 		helpNode=tmpNodes[i];
 		for(j=0; j<helpNode.pointers.length; j++){ 	
 			if(helpNode.pointers[j]!=undefined) tmpNodes.push(helpNode.pointers[j]);
-		}		
+			if(helpNode.pointers[j].rightPointer!=undefined) tmpNodes.push(helpNode.pointers[j].rightPointer);	
+		}	
 		i++;
 	}
 
@@ -140,7 +158,7 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 				}	
 			} 
 
-			if(tmpNodes[i]!=undefined && tmpNodes[i].parent!=undefined){ // jetzt die Pfeile
+			if(tmpNodes[i]!=undefined && tmpNodes[i].parent!=undefined && !tmpNodes[i].isOrphan){ // jetzt die Pfeile
 		
 				var parX=tmpNodes[i].parent.xPosition; // x-Wert des Elternknotens
 				var j;
@@ -149,7 +167,7 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 				}
 				parX = parX + cellWidth*j + 5; // jetzt steht der Startpunkt fest	
 				var line = new Kinetic.Line({
-					points: [tmpNodes[i].xPosition + cellWidth*this.model.order + 5, tmpNodes[i].yPosition, parX, tmpNodes[i].parent.yPosition + nodeHeight],
+					points: [tmpNodes[i].xPosition+cellWidth*this.model.order+5, tmpNodes[i].yPosition, parX, tmpNodes[i].parent.yPosition + nodeHeight],
 					stroke: 'black',
 					strokeWidth: 2*this.scale,
 					lineJoin: 'round',
@@ -158,57 +176,15 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 			}
 		}
 	}
-
-	var boxHeight=(this.model.history.length * 20 + 5)*this.scale; // Die Box mit dem Protokoll hinzufuegen
-	var distBottom=100*this.scale;
-/*
-	var protocolBox = new Kinetic.Rect({ 
-			x: 20*this.scale,
-			y: 20*this.scale,
-			width: 90*this.scale, 
-		     height: boxHeight,
-			stroke: 'black',
-			strokeWidth: 2*this.scale,
-	});
-	layer.add(protocolBox);
-
-
-	var val;
-	var hist, histOp, histVal, elCount;
-	clr="black";
-
-	for(i=0; i<this.model.history.length; i++){ // Die Zeilen der Protokoll-Box. Hier wird auf die Geschichte zugegriffen
-
-		hist=this.model.history[i];
-		histOp=hist.charAt(0);
-		histVal=hist.substr(2);
-
-		if(histOp=='r') clr = "red";
-		else if(histOp=='a') clr = "green";
-
-		val = new Kinetic.Text({
-			x: 25*this.scale,
-			y: 25*this.scale + i * 20*this.scale,
-			text: i+1 + ". " + histVal,
-			fontSize: 15*this.scale,
-			fontFamily: 'Calibri',
-			fill: clr,
-			width: 90*this.scale,
-		});
-		layer.add(val);
-	}*/
 			
 	var height=startPosY + (vertDist+2*nodeHeight)*(level+1);
-	//if(height<(boxHeight+distBottom)) height=boxHeight+distBottom;
 	this.stage.setWidth(startPosX + horDist + s*(nodeWidth+horDist) + 200 * this.scale); // Breite und Hoehe der Flaeche festlegen
 	this.stage.setHeight(height);
 	this.stage.removeChildren();
 	this.stage.add(layer);	
 
-	var hist, histOp, histVal, elCount, clr="black", protValues="";
-
+	var hist, histOp, histVal, elCount, clr="black", protValues=""; // Protokoll befuellen
 	for(i=0; i<this.model.history.length; i++){
-
 		hist=this.model.history[i];
 		histVal=hist.substr(2);
 		histOp=hist.charAt(0);
@@ -216,10 +192,9 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 		else if(histOp=='a') clr = "green";
 		elCount = i+1;
 		protValues = protValues + "<div style='color:" + clr + "'>" +  elCount + ". " + histVal +"</div>";
-
 	}
 
-	this.elementDisplay.innerHTML=protValues;
+	this.protDisplay.innerHTML=protValues;
 
 	if(actNode!=undefined){ // wandernder Kreis bei erster Zeichnung
 
@@ -227,19 +202,18 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 		if(op==1) clr="#66ff33"; // hinzufuegen
 		else if(op==2) clr="#ff6666"; // loeschen
 		else if(op==3) clr="#00ccff"; // suchen
-
 		var nValxPos=-15;
 		if(actVal<10) nValxPos=-5;
 		else if(actVal<100) nValxPos=-10;
 		
-		var circle = new Kinetic.Circle({
+		var circle = new Kinetic.Circle({ // Kreis
 			radius: 20*this.scale,
 			stroke: 'black',
 			strokeWidth: 2*this.scale,
 			fill: clr,
+			opacity: 0.5,
 		});
-
-		var nVal = new Kinetic.Text({
+		var nVal = new Kinetic.Text({ // Wert im Kreis
 			x: nValxPos*this.scale,
 			y: -5*this.scale,
 			text: actVal,
@@ -247,8 +221,7 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 			fontFamily: 'Calibri',
 			fill: 'black',
 		});
-
-		var ball = new Kinetic.Group({
+		var ball = new Kinetic.Group({ // Kreis + Wert
 			x: tmpNodes[0].xPosition + nodeWidth/2,
 			y: tmpNodes[0].yPosition - nodeHeight,	
 			draggable: true
@@ -259,38 +232,75 @@ BPlusTreeView.prototype.draw=function(actNode, actualValue, operation, contSec){
 		layer.add(ball);
 
 		var visitedNodes=[];
-		var tweens=[];
+		var xValues=[];
+		var yValues=[];
+		var dists=[];
+		var horWay=0;
+		var v=0, distHelp=0, distHelpX=0, distHelpY=0, helpX=0, helpY=0, distSum=0;
 		
 		for(i=tmpNodes.length-1; i>=0; i--){
 
 			helpNode=tmpNodes[i];
 			if(helpNode==actNode){ 
-				visitedNodes.push(helpNode);
+				visitedNodes.push(helpNode); 
 				while(helpNode.parent!=undefined && helpNode.parent!=this.model.root){
-					visitedNodes.unshift(helpNode.parent);
+					visitedNodes.unshift(helpNode.parent);// Alle Knoten auf dem Pfad speichern
 					helpNode=helpNode.parent;
 				}
 				finished=true;
 				break;
 			}
 		}
-		i=0;	
+
+		visitedNodes.unshift(this.model.root);
+
+		for(i=0; i<visitedNodes.length; i++){
+
+			helpNode=visitedNodes[i];
+			xValues.push(helpNode.xPosition + nodeWidth/2);
+			yValues.push(helpNode.yPosition - nodeHeight);
+
+			horWay=0;
+			for(v=0; v<helpNode.keys.length; v++) if(actVal<helpNode.keys[v]) break;
+			horWay=cellWidth*v+pointerSpace/2;
+	
+			if(v!=this.model.order){
+				xValues.push(helpNode.xPosition+horWay); // Punkt an der oberen Kante
+				yValues.push(helpNode.yPosition-nodeHeight); 
+			}
+			if(helpNode!=actNode){
+				xValues.push(helpNode.xPosition+horWay); // Punkt an der unteren Kante 
+				yValues.push(helpNode.yPosition);
+			}
+		}
+
+		for(i=1; i<xValues.length; i++){
+			distHelpX=xValues[i]-xValues[i-1];
+			distHelpY=yValues[i]-yValues[i-1];
+			distHelp=Math.sqrt(distHelpX*distHelpX+distHelpY*distHelpY);
+			distSum=distSum+distHelp;
+			dists.push(distHelp);
+		}
+
+		this.model.totalDur=distSum/moveSpeed*1000 + (xValues.length-1)*pauseTime;
+
+		i=1;
 		function moveCircle(){
 			var tween = new Kinetic.Tween({
 				node: ball,
-				x: visitedNodes[i].xPosition + nodeWidth/2,
-				y: visitedNodes[i].yPosition - nodeHeight,
-				duration: moveSpeed,
+				x: xValues[i],
+				y: yValues[i],
+				duration: dists[i-1]/moveSpeed,
 				onFinish: function(){
 					i++;
-					if(visitedNodes[i]!=undefined){
+					if(xValues[i]!=undefined){
 						setTimeout(function(){ 
 							moveCircle();
-						}, 1000);
+						}, pauseTime);
 					}
 				}
 			});
-			tween.play();
+			tween.play();	
 		}
 		if(tmpNodes.length>1) moveCircle();
 	}
